be35e9b587a8d1d9dd161e0453d962b2a89f5b3d
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-10-27 11:17:21 -0700

    fixed issues

--- console.py
+++ console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 from inspect import isclass
-import cmd
+import cmd, shlex, models
 
 """The HBNB command console"""
 
@@ -17,15 +17,22 @@ class HBNBCommand(cmd.Cmd):
             arg = BaseModel()
             print(arg.id)
 
-    def do_show(self, args):
+    def do_show(self, arg):
         """Prints the string representation of an instance based on the class name and id"""
-        if not args:
+        if not arg:
             print("** class name missing **")
-        if not isclass(args[0]):
-            print("** class doesn't exist **")
-        elif len(args) == 1:
-            print("** instance id missing **")
         else:
+            args = shlex.split(arg)
+            if not args[0] in globals():
+                print("** class doesn't exist **")
+            elif len(args) == 1:
+                print("** instance id missing **")
+            else:
+                key = args[0] + "." + args[1]
+                if key in models.storage.all():
+                    print(models.storage.all()[key])
+                else:
+                    print("** no instance found **")
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
